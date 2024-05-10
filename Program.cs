5be2befcf1323302e5a7da8c0742c9ec7a1ffd5c
つくも <92912896+Thukumo@users.noreply.github.com>
2024-05-10 08:06:52 +0000

    リファクタリング

--- Program.cs
+++ Program.cs
@@ -14,113 +14,69 @@ namespace ip
     public static bool IsValidGlobalIP(string ip)
     {
         string[] splitValues = ip.Split('.');
-        /*
-        if (string.IsNullOrWhiteSpace(ip))
+        var a = int.Parse(splitValues[0]);
+        var b = int.Parse(splitValues[1]);
+        switch (a)
         {
-            return false;
+            case 0:
+            case 10:
+            case 127:
+            case 169 when b == 254:
+            case 172 when 16 <= b && b <= 31:
+            case 192 when b == 168:
+                return false;
+            default:
+                return a < 224;
         }
-        if (splitValues.Length != 4)
-        {
-            return false;
-        }
-        if (!splitValues.All(r => byte.TryParse(r, out byte tempForParsing)))
-        {
-            return false;
-        }
-        */
-        if (splitValues[0] == "0")
-        {
-            return false;
-        }
-        if (splitValues[0] == "10")
-        {
-            return false;
-        }
-        if (splitValues[0] == "127")
-        {
-            return false;
-        }
-        if (splitValues[0] == "169" && splitValues[1] == "254")
-        {
-            return false;
-        }
-        if (splitValues[0] == "172" &&  16 <= int.Parse(splitValues[1]) && int.Parse(splitValues[1]) <= 31)
-        {
-            return false;
-        }
-        if (splitValues[0] == "192" && splitValues[1] == "168")
-        {
-            return false;
-        }
-        if(224 <= int.Parse(splitValues[0])) //224.0.0.0~はマルチキャストアドレス、240.~は実験用アドレス、255.~はブロードキャストアドレス
-        {
-            return false;
-        }
-        return true;
     }
-    public static async Task<bool> IsPortOpenAsync(string ip, int port, bool tcp)
+    public static async Task<string> IsPortOpenAsync(string ip, int port, bool tcp)
     {
         if(tcp)
         {
-            using var tcpClient = new System.Net.Sockets.TcpClient();
+            using var tcpClient = new TcpClient();
             try
             {
                 tcpClient.Connect(ip, port);
             }
-            catch (System.Net.Sockets.SocketException)
+            catch (SocketException)
             {
-                return false;
+                return "";
             }
-            return tcpClient.Client.Connected;
+            return tcpClient.Client.Connected? ip : "";
         }
         else
         {
             try
             {
-                var hoge = await new HttpClient{Timeout = TimeSpan.FromSeconds(30)}.GetAsync("http://" + ip + ":" + port + "/");
-                return hoge.IsSuccessStatusCode;
+                var client = await new HttpClient{Timeout = TimeSpan.FromSeconds(30)}.GetAsync("http://" + ip + ":" + port + "/");
+                return client.IsSuccessStatusCode? ip : "";
             }
-            catch (HttpRequestException)
+            catch(Exception ex) when (ex is HttpRequestException || ex is TaskCanceledException)
             {
-                return false;
-            }
-            catch(TaskCanceledException)
-            {
-                return false;
+                return "";
             }
         }
     }
-public static async Task<string> RetipAsync(string ip, int port, bool tcp = false) 
-{
-  if(await Task.Run(() => IsPortOpenAsync(ip, port, tcp)))
-  {
-    return ip; 
-  }
-  return "";
-}
     public static void Main(string[] args)
     {
-        int port = 80;
-        bool tcp = false;
-        for(int i = 0; i < args.Length; i++)
-        {
-            var hoge = args[i];
-            if(hoge == "TCP") tcp = true;
-                _ = int.TryParse(args[i], out port);
-        }
-        var lis = new List<int>();
-        for(int i = 0; i < 256; i++) lis.Add(i);
-        var ran = new Random();
-        Parallel.ForEach(lis.OrderBy(x => ran.Next()), new ParallelOptions(){MaxDegreeOfParallelism = 8}, i =>
-        {
-            var random = new Random();
-            string ip = "";
-            var tasks = new List<Task<string>>();
-            foreach(int j in lis.OrderBy(x => random.Next()))
+            int port = 80;
+            bool tcp = false;
+            foreach(var hoge in args)
             {
-                foreach(int k in lis.OrderBy(x => random.Next()))
+                if(hoge == "TCP") tcp = true;
+                _ = int.TryParse(hoge, out port);
+            }
+            //var lis = new List<int>();
+            //for(int i = 0; i < 256; i++) lis.Add(i);
+            var lis = Enumerable.Range(1, 255).ToArray();
+            Parallel.ForEach(lis.OrderBy(x => new Random().Next()), new ParallelOptions(){MaxDegreeOfParallelism = 8}, i =>
+            {
+                var random = new Random();
+                string ip = "";
+                var tasks = new List<Task<string>>();
+                foreach(int j in lis.OrderBy(x => random.Next())) foreach(int k in lis.OrderBy(x => random.Next()))
                 {
-                    for(int l = 0; l < 256; l++) if(IsValidGlobalIP(ip = $"{i}.{j}.{k}.{l}")) tasks.Add(RetipAsync(ip, port, tcp));
+                    for(int l = 0; l < 256; l++) if(IsValidGlobalIP(ip = $"{i}.{j}.{k}.{l}")) tasks.Add(IsPortOpenAsync(ip, port, tcp));
                     Task.WhenAll(tasks).Wait();
                     foreach(var task in tasks) if(task.Result != "")
                     {
@@ -129,8 +85,7 @@ public static async Task<string> RetipAsync(string ip, int port, bool tcp = fals
                     }
                     tasks.Clear();
                 }
-            }
-        });
-    }
+            });
+        }
     }
 }
