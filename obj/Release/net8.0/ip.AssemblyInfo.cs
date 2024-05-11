40b6d4d7927ef6cc8427e0311d9fa1d21c017ced
つくも <92912896+Thukumo@users.noreply.github.com>
2024-05-11 11:54:16 +0900

    ビルドしました

--- obj/Release/net8.0/ip.AssemblyInfo.cs
+++ obj/Release/net8.0/ip.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ip")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bd11ff536c1f21c1071a8fdf83b572004a526a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c60506aa0b7fef4a1c194454fe62c88da19b2182")]
 [assembly: System.Reflection.AssemblyProductAttribute("ip")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ip")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
