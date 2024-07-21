128928c9570057eb3fe1fb3b66ca1ed5795a5c43
つくも <92912896+Thukumo@users.noreply.github.com>
2024-07-21 21:34:46 +0900

    build

--- obj/Debug/net8.0/ip.AssemblyInfo.cs
+++ obj/Debug/net8.0/ip.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ip")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+651c298f1356ecd687964d0e34871e65f98df35d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81165525c2ea883edb66c31487b6c346251e75cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ip")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ip")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
